82cf173bae4cf966a342cf244563d9aead165842
Joachim Schlöffel <joachim.schloeffel@hausmed.de>
2012-10-21 13:17:28 +0200

    working draft

--- src/hm-bmi-calc.js
+++ src/hm-bmi-calc.js
@@ -1,8 +1,17 @@
 window.hmCalculateBMI = (function(){
-  var l = parseFloat(document.getElementById('hm-l'));
-  var m = parseFloat(document.getElementById('hm-m'));
-  var output = m/(l*l);
-  document.getElementById('hm-out').value=output;
+  var l = parseFloat(document.getElementById('hm-l').value) / 100;
+  var m = parseFloat(document.getElementById('hm-m').value);
+  if(l>0){
+    var output = m/(l*l);
+    document.getElementById('hm-out').value=Math.round(output * 10) / 10;
+  }
 });
+
+var _hm_wrapper = document.getElementById('hmBMI');
+_hm_wrapper.innerHTML="<h3>BMI-Rechner</h3><p>Übergewicht? Der Body-Mass-Index gibt Auskunft.</p>";
+_hm_wrapper.innerHTML += '<p><input placeholder="Gewicht" id="hm-m" type="number" min="30">Kg</p>';
+_hm_wrapper.innerHTML += '<p><input placeholder="Körpergröße" id="hm-l" type="number" min="120">cm</p>';
+_hm_wrapper.innerHTML += '<h3><label for="hm-out">Ihr BMI:</label></h3><p><output id="hm-out" value="0"></p>';
+
 document.getElementById('hm-l').onkeyup=hmCalculateBMI;
 document.getElementById('hm-m').onkeyup=hmCalculateBMI;
